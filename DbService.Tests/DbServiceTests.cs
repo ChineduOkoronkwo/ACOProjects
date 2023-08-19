f80a052a503de97ea2b31e4e0bf309e7eabf4990
Okoronkwo Alfred Chinedu <okoronkwoac@yahoo.com>
2023-08-19 19:09:59 -0400

    added excepton tests

--- DbService.Tests/DbServiceTests.cs
+++ DbService.Tests/DbServiceTests.cs
@@ -11,23 +11,39 @@ namespace DbService.Tests
 {
     public class DbServiceTests
     {
+        Mock<IQueryService> _mockQueryService;
+        IDbService<TestModel> _dbService;
+        string _testQuery = "test-query";
+        object[] _parm;
+        public DbServiceTests()
+        {
+            _mockQueryService = new();
+            _dbService = new DbService<TestModel>(_mockQueryService.Object);
+            var person = new TestModel();
+            _parm = new object[] {person};
+        }
+
         [Fact]
-        public async void Test1()
+        public async void TestExecuteAsync()
         {
             var expected = 1;
-            var person = new TestModel();
-            var testQuery = "test-query";
-            Mock<IQueryService> _mockQueryService = new();
             _mockQueryService.Setup(
                 s => s.ExecuteAsync(It.IsAny<string>(), It.IsAny<object>()))
                 .ReturnsAsync(expected);
-
-            IDbService<TestModel> _dbService = new DbService<TestModel>(_mockQueryService.Object);
-            object[] parm = {person!};
-            var result = await _dbService.CreateAsync(testQuery, parm);
+            var result = await _dbService.CreateAsync(_testQuery, _parm);
 
             Assert.Equal(expected, result);
-            _mockQueryService.Verify(s => s.ExecuteAsync(testQuery, parm), Times.Once);
+            _mockQueryService.Verify(s => s.ExecuteAsync(_testQuery, _parm), Times.Once);
+        }
+
+        [Fact]
+        public async void TestExecuteAsyncException() {
+            _mockQueryService.Setup(
+                s => s.ExecuteAsync(It.IsAny<string>(), It.IsAny<object>()))
+                .ThrowsAsync(new Exception("Test exception"));
+            await Assert.ThrowsAsync<Exception>(
+                async () => await _dbService.CreateAsync(_testQuery, _parm));
+            _mockQueryService.Verify(s => s.ExecuteAsync(_testQuery, _parm), Times.Once);
         }
     }
 
